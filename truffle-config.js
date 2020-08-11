d85d14f1f09e35f87c6ead44401508409665a17c
Kevin Bluer <kevin@bluer.com>
2020-08-11 14:50:19 -0500

    triggering teams build + trying something

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+
   networks: {
     teams: {
       url: `ws://marshmallow:8002/77f042b1-a657-49d2-b5eb-1d8facbcb333`,
